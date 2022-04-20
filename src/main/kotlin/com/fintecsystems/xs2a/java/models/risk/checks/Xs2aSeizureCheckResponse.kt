b94ae98b9f36282befa0b824a87c760445448763
Maik Mursall <mmursall@fintecsystems.com>
2022-04-20 17:00:13 +0200

    Changed type of Xs2aSeizureCheckResponse from Int to Boolean

--- src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aSeizureCheckResponse.kt
+++ src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aSeizureCheckResponse.kt
@@ -13,9 +13,9 @@ import java.time.LocalDateTime
  */
 
 data class Xs2aSeizureCheckResponse(
-    /* 1 if the customers account is a seizure account, 0 if otherwise */
+    /* True if the customers account is a seizure account, False if otherwise */
     @Json(name = "is_seizure")
-    var isSeizure: Int,
+    var isSeizure: Boolean,
     /* The limit of turnover days for this check */
     @Json(name = "days")
     var days: Int? = null,
