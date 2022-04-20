e1d70ab807f9df96afe3aeced566ba6ddab8e20d
Maik Mursall <mmursall@fintecsystems.com>
2022-04-20 16:56:52 +0200

    Added missing field to Xs2aRatingB2bCheck and Xs2aRatingB2cCheck

--- src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aRatingB2bCheck.kt
+++ src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aRatingB2bCheck.kt
@@ -1,3 +1,8 @@
 package com.fintecsystems.xs2a.java.models.risk.checks
 
-class Xs2aRatingB2bCheck
+import com.squareup.moshi.Json
+
+data class Xs2aRatingB2bCheck (
+    @Json(name = "days")
+    var days: Int? = null,
+)
--- src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aRatingB2cCheck.kt
+++ src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aRatingB2cCheck.kt
@@ -1,3 +1,8 @@
 package com.fintecsystems.xs2a.java.models.risk.checks
 
-class Xs2aRatingB2cCheck
+import com.squareup.moshi.Json
+
+data class Xs2aRatingB2cCheck  (
+    @Json(name = "days")
+    var days: Int? = null,
+)
