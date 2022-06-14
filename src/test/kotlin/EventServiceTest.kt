d6f1aa349defad94e6b1b7b799a2c4d54cc03832
Maik Mursall <mmursall@fintecsystems.com>
2022-06-14 12:29:10 +0200

    Reverted test

--- src/test/kotlin/EventServiceTest.kt
+++ src/test/kotlin/EventServiceTest.kt
@@ -1,13 +1,11 @@
-import com.fintecsystems.xs2a.java.helper.JsonSerializer
-import com.fintecsystems.xs2a.java.models.common.PaginatedList
 import com.fintecsystems.xs2a.java.models.events.EventObject
 import com.fintecsystems.xs2a.java.models.risk.Xs2aRisk
+import com.fintecsystems.xs2a.java.services.EventService
+import com.fintecsystems.xs2a.java.services.RiskService
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
-import com.fintecsystems.xs2a.java.services.EventService
-import com.fintecsystems.xs2a.java.services.RiskService
 import java.time.OffsetDateTime
 
 @TestMethodOrder(OrderAnnotation::class)
@@ -40,40 +38,8 @@ internal class EventServiceTest {
         }
     }
 
-    @Test
-    fun testBruh() {
-        val testString = """
-            {
-                "total": 0,
-                "perPage": 0,
-                "currentPage": 0,
-                "lastPage": 0,
-                "from": 0,
-                "to": 0,
-                "data": [
-                    {
-                        "id": "111111",
-                        "transaction": "100000",
-                        "type": "transaction.created",
-                        "data": null,
-                        "testmode": false,
-                        "message": "Nothing",
-                        "created_at": "2020-10-20"
-                    }
-                ]
-            }
-        """.trimIndent()
-
-        val intermediate = JsonSerializer.parseJson<Map<String, String>>(testString)
-        val test = intermediate[data]
-        println(test)
-        val value = PaginatedList.fromJson<EventObject>(intermediate)
-
-        println(value)
-    }
-
     companion object {
-        private val apiKey = System.getenv("API_KEY") ?: ""
+        private val apiKey = System.getenv("API_KEY")
 
         private lateinit var event: EventObject
     }
