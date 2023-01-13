9df61569b088fa493816383915e80d08c8f8ff7f
Maik Mursall <mmursall@fintecsystems.com>
2023-01-13 15:14:38 +0100

    Xs2aIncomeCheckResponse.data is now nullable

--- src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aIncomeCheckResponse.kt
+++ src/main/kotlin/com/fintecsystems/xs2a/java/models/risk/checks/Xs2aIncomeCheckResponse.kt
@@ -5,13 +5,13 @@ import java.time.OffsetDateTime
 
 /**
  *
- * @param data
+ * @param data Resulting data of this Check. May be null if no data is available.
  * @param createdAt Creation date of the object
  */
 
 data class Xs2aIncomeCheckResponse(
     @Json(name = "data")
-    var data: Xs2aIncomeCheckResponseData,
+    var data: Xs2aIncomeCheckResponseData? = null,
     /* Creation date of the object */
     @Json(name = "created_at")
     var createdAt: OffsetDateTime
